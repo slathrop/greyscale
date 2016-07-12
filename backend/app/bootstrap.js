e12bcb331b939b78b78bbe374111fa0954fdba7c
Sean Bolak <seanbolak@gmail.com>
2016-07-12 09:57:42 -0400

    added a pg.end() to initial connection. still hunting for unclosed queries.

--- backend/app/bootstrap.js
+++ backend/app/bootstrap.js
@@ -170,12 +170,12 @@ app.on('start', function () {
 
     var pgConString = 'postgres://' + pgUser + ':' + pgPassword + '@' + pgHost + ':' + pgPort;
 
-    pg.defaults.poolSize = 100;
     pg.connect(pgConString + '/' + pgDbName, function (err, client, done) {
         if (err) {
             debug('Could not connect to the database.');
         }
     });
+    pg.end();
 
     function startServer() {
         // Start server
